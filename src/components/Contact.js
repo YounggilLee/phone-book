632b9c38b60fa5c346cba7938676bf89747f2e47
GiL <iamleeyounggil@gmail.com>
2017-11-07 00:13:46 -0500

    Implement searching function

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,29 +1,52 @@
 import React, { Component } from 'react';
 import ContactInfo from './ContactInfo';
 
-export default class Contact extends React.Component{
+export default class Contact extends Component{
     constructor(props) {
       super(props);
       this.state = {
+          keyword: '',
         contactData:[
           {name:'A', phone:'000-0000-0000'},
            {name:'B', phone:'111-0000-0000'},
            {name:'C', phone:'222-0000-0000'}
         ]
       }
+
+      this.handleChange = this.handleChange.bind(this);
+
     }
     
+    handleChange(e) {
+        this.setState({
+            keyword: e.target.value
+        });
+    }
+
     render() {
       
-      const mapToComponent= (conData) => {
-        return conData.map((contact, i ) => {
+      const mapToComponent= (data) => {
+        console.log(data);
+            data.sort();
+            data = data.filter((contact) =>{
+                
+                return contact.name.toLowerCase().indexOf(this.state.keyword) >  -1;
+            } );
+        return data.map((contact, i ) => {
           return(<ContactInfo contact={contact} key={i}/>)
         })
       }
       
       return(
      <div>
-        {mapToComponent(this.state.contactData)}
+         <h1>Contacts</h1>
+         <input
+            name="keyword"
+            placeholder="Search"
+            value={this.state.keyword}
+            onChange={this.handleChange}
+         />
+        <div>{mapToComponent(this.state.contactData)}</div>
         </div>
       );
     }
--- src/components/ContactInfo.js
+++ src/components/ContactInfo.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 
-export default class ContactInfo extends React.Component{
+export default class ContactInfo extends Component{
     render() {
       return(
         <div>{this.props.contact.name} {this.props.contact.phone} </div>
